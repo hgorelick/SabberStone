0620bb94a3f5f3fad7ed12f444e4e45d010f8d3c
hgorelick <23178871+hgorelick@users.noreply.github.com>
2019-01-24 16:45:03 -0500

    Add files via upload

---
+++ SabberStoneCore/src/Model/Deck.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using SabberStoneCore.Enums;
+using SabberStoneCore.HearthQuery;
+
+namespace SabberStoneCore.Model
+{
+	public enum Archetype
+	{
+		INVALID = -1,
+		ZOO = 0,
+		ODD = 1,
+		EVEN = 2,
+		DEATHRATTLE = 3,
+		TAUNT = 4,
+		SECRET = 5,
+		QUEST = 6,
+		TOKEN = 7,
+		MILL = 8,
+		MIDRANGE = 9,
+		TEMPO = 10,
+		CONTROL = 11,
+		BIG = 12,
+		COMBO = 13,
+		CUBE = 14,
+		ELEMENTAL = 15,
+		FREEZE = 16,
+		KINGSBANE = 17,
+		MALYGOS = 18,
+		MECH = 19,
+		MECHATHUN = 20,
+		MURLOC = 21,
+		OTK = 22,
+		OVERLOAD = 23,
+		POGO = 24,
+		RECRUIT = 25,
+		RESURRECT = 26,
+		SHUDDERWOCK = 27,
+		SPELL = 28,
+		SPITEFUL = 29,
+		TURVY = 30,
+		CUSTOM = 31
+	}
+
+	/// <summary>
+	/// Defining members and constructors
+	/// </summary>
+	public partial class Deck : Collection<Card>
+	{
+		CardClass _heroClass { get; set; }
+		public CardClass HeroClass => _heroClass;
+
+		Archetype _archetype { get; set; }
+		public Archetype Archetype => _archetype;
+
+		string _deckName { get; set; }
+		public string DeckName => _deckName;
+
+		int _numGames { get; set; }
+		public int NumGames => _numGames;
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public Deck()
+		{
+			_heroClass = CardClass.INVALID;
+			_archetype = Archetype.INVALID;
+			_deckName = "new deck";
+			_numGames = 0;
+			Clear();
+		}
+
+		/// <summary>
+		/// Constructor that takes all deck information
+		/// </summary>
+		/// <param name="heroClass"></param>
+		/// <param name="archetype"></param>
+		/// <param name="deckName"></param>
+		/// <param name="num_games"></param>
+		/// <param name="cards"></param>
+		public Deck(CardClass heroClass = CardClass.INVALID, Archetype archetype = Archetype.INVALID, string deckName = "new deck", int num_games = 0, List<Card> cards = null)
+		{
+			_heroClass = heroClass;
+			_archetype = archetype;
+			_deckName = deckName;
+			_numGames = num_games;
+
+			if (cards != null)
+			{
+				Clear();
+				this.AddRange(cards);
+			}
+		}
+		bool OpponentSet { get; set; } = false;
+
+		/// <summary>
+		/// Overloaded constructor that extracts the archetype from the deck's name
+		/// </summary>
+		/// <param name="heroClass"></param>
+		/// <param name="archetype"></param>
+		/// <param name="deckName"></param>
+		/// <param name="num_games"></param>
+		/// <param name="cards"></param>
+		public Deck(CardClass heroClass = CardClass.INVALID, string deckName = "new deck", int num_games = 0, List<Card> cards = null)
+		{
+			_heroClass = heroClass;
+
+			try { _archetype = DeckHelper.Map<Archetype>(deckName.Split('_')[0]); }
+
+			catch
+			{
+				if (deckName == "IceFireWarrior")
+					_archetype = Archetype.QUEST;
+				else
+					_archetype = Archetype.CUSTOM;
+			}
+
+			_deckName = deckName;
+			_numGames = num_games;
+
+			if (cards != null)
+			{
+				Clear();
+				this.AddRange(cards);
+			}
+		}
+
+		/// <summary>
+		/// Creates the most popular deck of the given hero class
+		/// </summary>
+		/// <param name="heroClass"></param>
+		public Deck(CardClass heroClass)
+		{
+			_heroClass = heroClass;
+
+			var deckQuery = new DeckQuery(heroClass);
+
+			var deck = new Deck(deckQuery.GetMostPopular());
+
+			_archetype = deck.Archetype;
+			_deckName = deck.DeckName;
+			_numGames = deck.NumGames;
+
+			Clear();
+			this.AddRange(deck.ToList());
+		}
+
+		/// <summary>
+		/// Copy constructor
+		/// </summary>
+		/// <param name="other"></param>
+		Deck(Deck other)
+		{
+			_heroClass = other.HeroClass;
+			_archetype = other.Archetype;
+			_deckName = other.DeckName;
+			_numGames = other.NumGames;
+
+			Clear();
+			this.AddRange(other.ToList());
+		}
+
+		/// <summary>
+		/// Calls the copy constructor
+		/// </summary>
+		/// <returns></returns>
+		public Deck Clone() { return new Deck(this); }
+	}
+
+	/// <summary>
+	/// Getters, helper methods, and Collection<T> overrides
+	/// </summary>
+	public partial class Deck : Collection<Card>
+	{
+		/// <summary>
+		/// Returns a deck from a sqlite database
+		/// </summary>
+		/// <param name="deckName"></param>
+		public Deck FromDB(CardClass heroClass, Deck deck = null, bool max = false, string deckName = "none")
+		{
+			var deckQuery = new DeckQuery(heroClass);
+
+			if (deck != null)
+			{
+				//try { return deckQuery.AddToDB(deck); }
+
+				//catch (Exception)
+				//{
+				return deck;
+				//}
+			}
+
+			else if (max)
+				return new Deck(deckQuery.GetMostPopular());
+
+			else if (deckName != "none")
+				return deckQuery.DeckFromName(deckName);
+
+			throw new Exception($"There was a problem retrieving the deck from the database, or the arguments are incorrect");
+		}
+	}
+
+	public static class DeckHelper
+	{
+		/// <summary>
+		/// Returns the archetype that matches the given string
+		/// </summary>
+		/// <param name="archetype"></param>
+		/// <returns></returns>
+		public static T Map<T>(this string value)
+		{
+			if ((String.IsNullOrEmpty(value) == false) && typeof(T).IsEnum)
+			{
+				try { return (T)(Enum.Parse(typeof(T), value, true)); }
+				catch { }
+			}
+			throw new InvalidCastException();
+		}
+
+		/// <summary>
+		/// Collection<Card> override
+		/// </summary>
+		/// <typeparam name="Card"></typeparam>
+		/// <param name="dest"></param>
+		/// <param name="source"></param>
+		public static void AddRange(this Collection<Card> dest, List<Card> source)
+		{
+			foreach (Card card in source)
+				dest.Add(card);
+		}
+
+		/// <summary>
+		/// Remove<> override
+		/// </summary>
+		/// <param name="p"></param>
+		public static void Remove(this Deck d, Func<Card, bool> p)
+		{
+			foreach (Card c in d)
+				if (p(c))
+				{
+					d.Remove(c);
+					return;
+				}
+		}
+
+		public static void ForEach(this Deck d, Action<Card> a)
+		{
+			foreach (Card c in d)
+				a(c);
+		}
+	}
+};
---
+++ SabberStoneCore/src/Model/DeckQuery.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using SabberStoneCore.Model;
+using SabberStoneCore.Model.Entities;
+using SabberStoneCore.Enums;
+
+namespace SabberStoneCore.HearthQuery
+{
+	public class DeckQuery
+	{
+		#region SQL Members
+		const string connection = @"DataSource=C:\Users\hgore\SabberStone\SabberStoneCore\resources\Data\hearth_decks.db;";
+		DataTable Deck = new DataTable();
+		DataTable DeckNames = new DataTable();
+		DataTable DeckCards = new DataTable();
+		#endregion
+
+		CardClass _heroClass { get; set; }
+		public CardClass HeroClass => _heroClass;
+
+		public DeckQuery(CardClass heroClass) { _heroClass = heroClass; }
+
+		/// <summary>
+		/// Returns the deck with the most number of games played from hearth_decks.db
+		/// </summary>
+		/// <returns></returns>
+		public Deck GetMostPopular()
+		{
+			string query = $"select * from AllInfo where num_games = (select max(num_games) from AllInfo where hero_class = '{HeroClass.ToString()}');";
+			Deck = new DataTable();
+
+			using (var conn = new SQLiteConnection(connection))
+			{
+				var cmd = new SQLiteCommand(query, conn);
+				using (var adapter = new SQLiteDataAdapter { SelectCommand = cmd })
+				{
+					adapter.Fill(Deck);
+				}
+			}
+
+			var cards = new List<Card>();
+			//var col = Deck.Columns[4];
+
+			foreach (DataRow row in Deck.Rows)
+				cards.Add(Cards.FromName(row.ItemArray[4].ToString()));
+
+			Archetype archetype = DeckHelper.Map<Archetype>(Deck.Rows[0].ItemArray[1].ToString());
+			string deckName = Deck.Rows[0].ItemArray[2].ToString();
+			int numGames = (int)(long)Deck.Rows[0].ItemArray[3];
+
+			return new Deck(HeroClass, archetype, deckName, numGames, cards);
+		}
+
+		/// <summary>
+		/// Returns the specified deck from hearth_decks.db
+		/// </summary>
+		/// <param name="deckName"></param>
+		/// <returns></returns>
+		public Deck DeckFromName(string deckName)
+		{
+			string query = $"select archetype, num_games, card_name from AllInfo where deck_name = '{deckName}';";
+			Deck = new DataTable();
+
+			using (var conn = new SQLiteConnection(connection))
+			{
+				conn.Open();
+				var cmd = new SQLiteCommand(query, conn);
+				using (var adapter = new SQLiteDataAdapter { SelectCommand = cmd })
+				{
+					adapter.Fill(Deck);
+				}
+			}
+
+			var cards = new List<Card>();
+			//var col = Deck.Columns[4];
+
+			foreach (DataRow row in Deck.Rows)
+				cards.Add(Cards.FromName(row.ItemArray[2].ToString()));
+
+			Archetype archetype = DeckHelper.Map<Archetype>(Deck.Rows[0].ItemArray[0].ToString());
+			int numGames = (int)(long)Deck.Rows[0].ItemArray[1];
+
+			return new Deck(HeroClass, archetype, deckName, numGames, cards);
+		}
+
+		/// <summary>
+		/// Returns the deck from hearth_decks.db with the most matching cards in psf.
+		/// Can be filtered to only search through the decks with names in possible.
+		/// </summary>
+		/// <param name="psf"></param>
+		/// <param name="possible"></param>
+		/// <returns></returns>
+		public List<string> GetBestMatch(List<Card> psf, List<string> possible = null)
+		{
+			string query = $"select distinct deck_name from AllInfo where hero_class = '{HeroClass}';";
+			DeckNames = new DataTable();
+
+			var matches = new List<string>();
+			using (var conn = new SQLiteConnection(connection))
+			{
+				conn.Open();
+				List<string> names = GetDeckNames();
+
+				int matchCount = 0;
+				foreach (string name in names)
+				{
+					query = $"select card_name from deck_cards where deck_name = '{name}';";
+					DeckCards = new DataTable();
+
+					var cmd = new SQLiteCommand(query, conn);
+					using (var adapter = new SQLiteDataAdapter { SelectCommand = cmd })
+					{
+						adapter.Fill(DeckCards);
+					}
+
+					var cards = new List<string>();
+					foreach (DataRow row in DeckCards.Rows)
+					{
+						string card = row.ItemArray[0].ToString();
+						if (psf.Contains(c => c.Name == card))
+							cards.Add(card);
+					}
+
+					if (cards.Count >= matchCount)
+					{
+						if (cards.Count > matchCount)
+							matches.Clear();
+
+						matchCount = cards.Count;
+						matches.Add(name);
+					}
+				}
+			}
+			return matches;
+		}
+
+		/// <summary>
+		/// Returns a list of each deck's name in this HeroClass
+		/// </summary>
+		/// <returns></returns>
+		List<string> GetDeckNames()
+		{
+			string query = $"select distinct deck_name from AllInfo where hero_class = '{HeroClass}';";
+			DeckNames = new DataTable();
+
+			using (var conn = new SQLiteConnection(connection))
+			{
+				conn.Open();
+				var cmd = new SQLiteCommand(query, conn);
+				using (var adapter = new SQLiteDataAdapter { SelectCommand = cmd })
+				{
+					adapter.Fill(DeckNames);
+				}
+			}
+
+			var names = new List<string>();
+			foreach (DataRow row in DeckNames.Rows)
+				names.Add(row.ItemArray[0].ToString());
+
+			return names;
+		}
+	}
+
+	internal static class QHelper
+	{
+		internal static bool Contains(this List<Card> l, Func<Card, bool> p)
+		{
+			foreach (Card c in l)
+			{
+				if (p(c))
+					return true;
+			}
+			return false;
+		}
+	}
+}
